d2dc6c663ac6e8545ebd05d1eaf8d974d8d8706c
LilAliTech <lilalix20@gmail.com>
2022-12-18 22:47:39 +0000

    Main

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
       <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between dark:text-white">
             
-            <ul className=" 	align-items: flex-end;">
+            <ul className="flex items-center  ;">
               <li>
                 <MdModeNight
                   onClick={() => setDarkMode(!darkMode)}
@@ -29,7 +29,7 @@ export default function Home() {
               </li>
               <li>
                 <a
-                  className="bg-gradient-to-r from-cyan-500 text- to-teal-500 hover:from-blue-500 hover:to-indigo-500 text-white px-4 py-2 border-none rounded-md ml-8"
+                  className=" bg-gradient-to-r from-cyan-500 text- to-teal-500 hover:from-blue-500 hover:to-indigo-500 text-white px-4 py-2 border-none rounded-md ml-8"
                   href="#"
                 >
                   Resume
